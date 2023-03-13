3dc02c6e570b5e5061223483b6401608273e4ae3
Antoine Mandin <antoine.mandin@sirehna.com>
2023-03-13 19:00:50 +0100

    WIP

--- gitlab_stats.js
+++ gitlab_stats.js
@@ -21,9 +21,61 @@ const buildIssuesStatistics = (dialog, issues) => {
         <li>Total number of issues: ${issueCount}</li>
         <li>Number of closed issues: ${issueClosedCount}</li>
     </ul>
+    <div id="chart"></div>
     `
 
     dialog.appendChild(container)
+
+    const issuesCreated = {
+        name: "Created issues",
+        x: [],
+        y: [],
+        mode: 'lines'
+    };
+    let amount = 0
+    for (let issue of issues.sort((i1, i2) => new Date(i1.created_at) - new Date(i2.created_at))) {
+        issuesCreated.x.push(new Date(issue.created_at))
+        issuesCreated.y.push(++amount)
+    }
+
+    const issuesClosed = {
+        name: "Closed issues",
+        x: [],
+        y: [],
+        mode: 'lines'
+    };
+    amount = 0
+    for (let issue of issues.filter(issue => issue.closed_at !== null).sort((i1, i2) => new Date(i1.closed_at) - new Date(i2.closed_at))) {
+        issuesClosed.x.push(new Date(issue.closed_at))
+        issuesClosed.y.push(++amount)
+    }
+
+    const issuesOpen = {
+        name: "Open issues",
+        x: [],
+        y: [],
+        mode: 'lines'
+    };
+    const events = []
+    for (let issue of issues) {
+        events.push({type: "open", date: new Date(issue.created_at)})
+        if (issue.closed_at !== null) {
+            events.push({type: "close", date: new Date(issue.closed_at)})
+        }
+    }
+    amount = 0
+    for (let event of events.sort((e1, e2) => e1.date - e2.date)) {
+        issuesOpen.x.push(event.date)
+        if (event.type === "open") {
+            amount++
+        } else {
+            amount--
+        }
+        issuesOpen.y.push(amount)
+    }
+
+
+    Plotly.newPlot('chart', [issuesCreated, issuesClosed, issuesOpen], {title:'Opened/Closed issues across time'});
 }
 
 const populateDialog = (dialog, api, projectId) => {
@@ -41,39 +93,34 @@ const populateDialog = (dialog, api, projectId) => {
 const addDialogElement = () => {
     const container = document.querySelector("body")
 
-    const dialogBack = document.createElement("div")
-    dialogBack.style.position = "fixed"
-    dialogBack.style.left = 0
-    dialogBack.style.width = "100%"
-    dialogBack.style.top = 0
-    dialogBack.style.height = "100%"
-    dialogBack.style.zIndex = 999
-    dialogBack.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
-    dialogBack.style.display = "none"
-    container.appendChild(dialogBack)
-    dialogBack.onclick = () => {
-        dialogBack.style.display = "none"
-    }
-
-    const dialog = document.createElement("div")
-    dialog.style.margin = "10%"
+    const dialog = document.createElement("dialog")
+    dialog.style.position = "fixed"
+    dialog.style.left = "10%"
+    dialog.style.top = "10%"
     dialog.style.width = "80%"
     dialog.style.height = "80%"
     dialog.style.backgroundColor = "rgb(41, 40, 45)"
     dialog.style.borderRadius = "5px"
-    dialog.style.padding = "2em"
-    dialogBack.appendChild(dialog)
+    dialog.style.padding = "0"
+    dialog.style.margin = "0"
+    dialog.onclick = (e) => {
+        if (e.target.nodeName === 'DIALOG') {
+            dialog.close('dismiss')
+        }
+    }
+    container.appendChild(dialog)
 
-    dialog.show = (api, projectId) => {
-        dialogBack.style.display = "block"
-        populateDialog(dialog, api, projectId)
+    const content = document.createElement("form")
+    content.style.padding = "2em"
+    content.method = "dialog"
+    dialog.appendChild(content)
+
+    content.show = (api, projectId) => {
+        dialog.showModal()
+        populateDialog(content, api, projectId)
     }
 
-    dialog.hide = () => {
-        dialogBack.style.display = "none"
-    }
-
-    return dialog
+    return content
 }
 
 
